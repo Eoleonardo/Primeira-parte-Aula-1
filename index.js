d019d02ad65bba7f8e21e0ee426d2be7796b9ed7
Eo.leo silva <101286236+Eoleonardo@users.noreply.github.com>
2022-03-09 18:53:42 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+class Estudante{
+  constructor(nome,sobrenome,turma,estituicão,data_nascimento){
+    this.nome = nome;
+  this.sobrenome = sobrenome;
+  this.turma = turma;
+  this.estituicão = estituicão;
+  this.data_nascimento = data_nascimento;
+  }
+}
+let estudante = new Estudante("leo","silva","3B","ifms","08/09/2006");
+console.log(estudante.nome)
+console.log(estudante.sobrenome)
+console.log(estudante.turma)
+console.log(estudante.estituicão)
+console.log(estudante.data_nascimento)
+
+
+
